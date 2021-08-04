2899675c55e69fa64e994c15efdf724fc0ca0847
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-08-05 00:44:11 +0200

    Update process.php

--- process.php
+++ process.php
@@ -1,6 +1,20 @@
+//<?php
+//require_once('main.php');
+//$database=new Database();
+//echo $database;
+//}
+//?>
 <?php
-require_once('main.php');
-$database=new Database();
-echo $database;
+$servername = "10.82.144.4";
+$username = "root";
+$password = "password";
+$dbname="mytravelrental_RBAC";
+
+$response=array();
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
