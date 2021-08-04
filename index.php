50cdcf3f45feead50319ec105b4f1a3ed88638f7
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2021-08-05 01:12:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    //include 'process.php';    
-    require_once('main.php');
-    $database=new Database();
-    echo $database;
+    include 'process.php';    
+    //require_once('main.php');
+    //$database=new Database();
+    //echo $database;
 ?>
