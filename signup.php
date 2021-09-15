c59e140f7046aad7ea7f1980d6d3f521b0316208
LidyaNuramo <lidyagnuramo@gmail.com>
2021-09-16 01:45:57 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -99,6 +99,21 @@
     <script>
     $(".preloader").fadeOut();
     </script>
+    <script src="../../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../../assets/extra-libs/sparkline/sparkline.js"></script>
+    <script src="../../dist/js/waves.js"></script>
+    <script src="../../dist/js/sidebarmenu.js"></script>
+    <script src="../../dist/js/custom.min.js"></script>
+    <script src="../../assets/libs/inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
+    <script src="../../dist/js/pages/mask/mask.init.js"></script>
+    <script src="../../assets/libs/select2/dist/js/select2.full.min.js"></script>
+    <script src="../../assets/libs/select2/dist/js/select2.min.js"></script>
+    <script src="../../assets/libs/jquery-asColor/dist/jquery-asColor.min.js"></script>
+    <script src="../../assets/libs/jquery-asGradient/dist/jquery-asGradient.js"></script>
+    <script src="../../assets/libs/jquery-asColorPicker/dist/jquery-asColorPicker.min.js"></script>
+    <script src="../../assets/libs/jquery-minicolors/jquery.minicolors.min.js"></script>
+    <script src="../../assets/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+    <script src="../../assets/libs/quill/dist/quill.min.js"></script>
 
 </body>
 
