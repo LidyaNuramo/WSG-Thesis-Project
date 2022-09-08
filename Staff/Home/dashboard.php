a4318668fe8bf6c7dfa0a964ac66f87e5e8c6a79
LidyaNuramo <lidyagnuramo@gmail.com>
2022-09-08 05:07:20 +0200

    .

--- Staff/Home/dashboard.php
+++ Staff/Home/dashboard.php
@@ -133,7 +133,7 @@
                                 <div class="card-body">
                                     <div class="d-md-flex align-items-center">
                                         <div>
-                                            <h4 class="card-title">Site Analysis</h4>
+                                            <h4 class="card-title">Assets Analysis</h4>
                                             <h5 class="card-subtitle">Overview of Latest Month</h5>
                                         </div>
                                     </div>
@@ -145,50 +145,7 @@
                                             </div>
                                         </div>
                                         <div class="col-lg-3">
-                                            <div class="row">
-                                                <div class="col-6">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-user mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">2540</h5>
-                                                        <small class="font-light">Total Users</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-plus mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">120</h5>
-                                                        <small class="font-light">New Users</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 mt-3">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-cart-plus mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">656</h5>
-                                                        <small class="font-light">Total Shop</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 mt-3">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-tag mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">9540</h5>
-                                                        <small class="font-light">Total Orders</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 mt-3">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-table mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">100</h5>
-                                                        <small class="font-light">Pending Orders</small>
-                                                    </div>
-                                                </div>
-                                                <div class="col-6 mt-3">
-                                                    <div class="bg-dark p-10 text-white text-center">
-                                                        <i class="fa fa-globe mb-1 font-16"></i>
-                                                        <h5 class="mb-0 mt-1">8540</h5>
-                                                        <small class="font-light">Online Orders</small>
-                                                    </div>
-                                                </div>
-                                            </div>
+                                            <? echo 'https://lidyagnuramo.grafana.net/d/wnSDH0M4k/mytravelrental-dashboard?orgId=1&from=1662584632921&to=1662606232921&theme=dark&viewPanel=2' ?>
                                         </div>
                                         <!-- column -->
                                     </div>
