d2108175713558485c19bd49592c309a75fe71ca
LidyaNuramo <48695185+LidyaNuramo@users.noreply.github.com>
2022-09-18 03:16:32 +0200

    cloud storage

---
+++ DB/cloudstorage.php
@@ -0,0 +1,25 @@
+require 'vendor/autoload.php';
+
+use Google\Cloud\Storage\StorageClient;
+
+// Providing the Google Cloud project ID.
+$storage = new StorageClient([
+    'projectId' => 'wsg-thesis-project-359600'
+]);
+
+$bucket->upload(
+    fopen('/data/file.txt', 'r')
+);
+
+// Using Predefined ACLs to manage object permissions, you may
+// upload a file and give read access to anyone with the URL.
+$bucket->upload(
+    fopen('/data/file.txt', 'r'),
+    [
+        'predefinedAcl' => 'publicRead'
+    ]
+);
+
+// Download and store an object from the bucket locally.
+$object = $bucket->object('file_backup.txt');
+$object->downloadToFile('/data/file_backup.txt');
