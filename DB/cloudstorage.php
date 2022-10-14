aec8ffe12066ed2cb489670412dc9126e26c0f99
LidyaNuramo <lidyagnuramo@gmail.com>
2022-10-14 03:01:14 +0200

    bucket

--- DB/cloudstorage.php
+++ DB/cloudstorage.php
@@ -1,3 +1,5 @@
+<?php
+
 require 'vendor/autoload.php';
 
 use Google\Cloud\Storage\StorageClient;
@@ -7,19 +9,23 @@ $storage = new StorageClient([
     'projectId' => 'wsg-thesis-project-359600'
 ]);
 
+$bucket = $storage->bucket('mytravelrental-bucket');
+
 $bucket->upload(
-    fopen('/data/file.txt', 'r')
+    fopen('cloudsql.php', 'r')
 );
 
 // Using Predefined ACLs to manage object permissions, you may
 // upload a file and give read access to anyone with the URL.
-$bucket->upload(
-    fopen('/data/file.txt', 'r'),
-    [
-        'predefinedAcl' => 'publicRead'
-    ]
-);
+// $bucket->upload(
+//     fopen('/data/file.txt', 'r'),
+//     [
+//         'predefinedAcl' => 'publicRead'
+//     ]
+// );
 
 // Download and store an object from the bucket locally.
-$object = $bucket->object('file_backup.txt');
-$object->downloadToFile('/data/file_backup.txt');
+// $object = $bucket->object('file_backup.txt');
+// $object->downloadToFile('/data/file_backup.txt');
+
+?>
