5b1592105df9807937585f3cdd928da9874bc838
LidyaNuramo <lidyagnuramo@gmail.com>
2022-10-14 03:21:21 +0200

    .

--- DB/cloudstorage.php
+++ DB/cloudstorage.php
@@ -4,16 +4,16 @@ require '../vendor/autoload.php';
 
 use Google\Cloud\Storage\StorageClient;
 
-// Providing the Google Cloud project ID.
-$storage = new StorageClient([
-    'projectId' => 'wsg-thesis-project-359600'
-]);
+// // Providing the Google Cloud project ID.
+// $storage = new StorageClient([
+//     'projectId' => 'wsg-thesis-project-359600'
+// ]);
 
-$bucket = $storage->bucket('mytravelrental-bucket');
+// $bucket = $storage->bucket('mytravelrental-bucket');
 
-$bucket->upload(
-    fopen('cloudsql.php', 'r')
-);
+// $bucket->upload(
+//     fopen('cloudsql.php', 'r')
+// );
 
 // Using Predefined ACLs to manage object permissions, you may
 // upload a file and give read access to anyone with the URL.
