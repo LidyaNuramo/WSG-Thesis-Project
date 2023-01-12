4927fa7cab18a9013a999e85205607d00597d373
LidyaNuramo <lidyagnuramo@gmail.com>
2023-01-12 08:33:53 +0100

    sql ip

--- DB/cloudsql.php
+++ DB/cloudsql.php
@@ -4,15 +4,12 @@ class Database
 {	
 	private $connection = null;
 	private $results = array();
-	private $host = "localhost";
-	// private $db = "id17747167_mytravelrental";
-	// private $user = "id17747167_root";
-	// private $password = "2^vR_Z0jye9#rCy7";
+	// private $host = "localhost";
 	// private $host = "34.140.88.200";
-	// private $host = "10.122.64.4";
+	private $host = "10.251.32.3";
 	private $db = "mytravelrental";
 	private $user = "root";
-	// private $password = "/m8=2?Ve;g.Hr?uc";
+	// private $password = "&v{+ikY$*xKTM^n\";
 	private $password = "";
 	private $db_prefix = "";
 
