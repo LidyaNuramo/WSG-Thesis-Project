8fbc23b5e427171abe00562e20554cb09d02e63e
LidyaNuramo <lidyagnuramo@gmail.com>
2023-01-23 23:40:45 +0100

    .

--- DB/cloudstorage.php
+++ DB/cloudstorage.php
@@ -42,10 +42,10 @@ class Bucket{
     }
 
     private function storage_client($key_file) {
-        // $storage = new StorageClient([
-        //     'keyFile' => json_decode(file_get_contents($key_file), true)
-        // ]);
-        $storage = new StorageClient();
+        $storage = new StorageClient([
+            'keyFile' => json_decode(file_get_contents($key_file), true)
+        ]);
+        // $storage = new StorageClient();
         if (!$storage) {
             echo " Cloud Storage Connection error.";
             exit;
